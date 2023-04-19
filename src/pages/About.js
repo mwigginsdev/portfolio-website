60f72b9316b5ef2e31cff020d132d99cd3a10977
vicarious <>
2023-04-18 21:10:19 -0500

    Add mobile screen check hook and layout properly for mobile

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,27 +1,27 @@
+import useCheckMobileScreen from "../hooks/useCheckMobileScreen";
+
+
 export default function About() {
+    const isMobile = useCheckMobileScreen();
     return (
       <div className="App">
         <header className="App-header"/>
-        <h1 class="Text-default">Hey, I'm Matt Wiggins!</h1>
-        <body class="Text-default">
-          I am a full stack mobile engineer based in Illinois. I'm currently working
-        </body>
-        <body class="Text-default">
-        at Casa leading a team of talented mobile engineers in building our primary
-        </body>
-        <body class="Text-default">
-          mobile product as well as attending U of I for a Master's in CS.
-        </body>
-        <body class="Text-default" style={{ marginTop: 14 }}>
-          In the past I have worked as a mobile developer for companies such as
-        </body>
-        <body class="Text-default">
-          John Deere, Axion.network, and The Climate Corporation.
-        </body>
-
-        <body class="Text-default" style={{ marginTop: 14 }}>
+        <div style={{
+          textAlign: 'center',
+          width: isMobile ? '90%' : '40%',
+        }}>
+          <h1 class="Text-default">Hey, I'm Matt Wiggins!</h1>
+          <p class="Text-default">
+          I am a full stack mobile engineer based in Illinois, currently working at Casa leading a team of talented mobile engineers in building our primary mobile product.
+          In the past I have worked as a mobile developer for companies such as John Deere, Axion.network, and The Climate Corporation.
+          </p>
+          <p class="Text-default">
+            I'm also attending the University of Illinois for a Master's in CS because I have a passion for learning and teaching.
+          </p>
+          <p class="Text-default">
           Check out what I've been building in the <a class="Link-default" href="https://www.wiggins.dev/work">Work</a> or <a class="Link-default" href="https://www.wiggins.dev/projects">Projects</a> pages!
-        </body>
+          </p>
+        </div>
       </div>
     );
   }
